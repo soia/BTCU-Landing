22757d0c7a6c01b8c1ee6a4a4cb5feb219ddcc16
Nikita Soia <nikita.soya@gmail.com>
2021-01-12 14:40:57 +0200

    start mobile layout

---
+++ release-deploy.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line
+const Deployer = require('ssh-deploy-release');
+
+const options = {
+    localPath: 'build',
+    host: 'devnew.btcu.io',
+    username: 'front-dev-2',
+    password: 'qzahxynw1BlV1jTUcr2zgoQ6',
+    deployPath: '/home/front-dev-2',
+    port: '22045',
+    releasesToKeep: 3,
+    currentReleaseLink: 'btcu.io',
+    onBeforeLink: context => `chmod -R g+rx ${context.release.path}`,
+};
+
+const deployer = new Deployer(options);
+deployer.deployRelease(() => {
+    // console.log('Ok !')
+});
